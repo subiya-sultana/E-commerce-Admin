f21b4f72d270dc774cae8c36c70c07faa098cced
subiya-sultana <itsmesubiya@gmail.com>
2024-12-14 16:49:07 +0530

    new updates

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -6,7 +6,8 @@ export default function ProductForm({
         _id,
         title: existingTitle,
         description: existingDescription,
-        price : existingPrice
+        price : existingPrice,
+        images
     }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
@@ -31,6 +32,17 @@ export default function ProductForm({
     if(goToProductsPage == true){
         return router.push('/products');
     }
+    async function uploadImages(e){
+        console.log(e)
+        const files = e.target?.files;
+        if(files?.length > 0){
+            const data = new FormData();
+            files.forEach(file => {
+                data.append("file",file)
+            });
+            const res = await axios.post('/api/uploadFile', data)
+        }
+    }
 
     return(
         <form onSubmit={AddProduct}>
@@ -42,6 +54,20 @@ export default function ProductForm({
                 onChange={e => setTitle(e.target.value)}
                 required
             />
+            <label>Product Images</label>
+            <div className="mb-2">
+                <label className="cursor-pointer w-24 h-24 bg-gray-300 text-gray-500 text-center flex flex-col items-center justify-center rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>Upload</div>
+                    <input type="file" className="hidden" onChange={uploadImages}/>
+                </label>
+                {
+                    !images?.length && (<div>No photos available to preview this product</div>)
+
+                }
+            </div>
             <label>Description</label>
             <textarea 
                 placeholder="Product Description"
@@ -58,9 +84,6 @@ export default function ProductForm({
                 required
             />
             <button type="submit" className="btn-primary">Save</button>
-            
-            <p>{title} and {description} and {price}</p>
-
         </form>
     )
 }
