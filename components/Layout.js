e6a0bb13fb941df44f9601229c45cd08d7263a7a
subiya-sultana <itsmesubiya@gmail.com>
2025-01-21 07:23:13 +0530

    navbar responsive

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 // Layout component... cosists of sideNav and main div
 import { useSession, signIn, signOut } from "next-auth/react"
 import SideNav from "@/components/sideNav";
+import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({children}) {
+
+  const [showNav, setShowNav] = useState(false)
+
   // handling user login
   const { data: session } = useSession()
   if(!session) {
@@ -17,9 +22,21 @@ export default function Layout({children}) {
     )
   }
   return (
-    <div className="bg-green-900 w-screen min-h-screen flex">
-      <SideNav />
-      <div className="bg-green-100 flex-grow m-2 ml-0 p-3 rounded-lg ">{children}</div>
+    <div className="bg-green-900 w-screen min-h-screen">
+      <div className="md:hidden flex items-center p-2">
+        <button onClick={()=>setShowNav(true)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 text-white">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <SideNav show={showNav} setShow={setShowNav}/>
+        <div className="bg-green-100 flex-grow m-2 p-3 rounded-lg min-h-[80%]">{children}</div>
+      </div>
     </div>
   );
 }
